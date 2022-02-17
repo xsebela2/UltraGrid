d3250f84d7a2c1f211779da2b9ede03fd095d6bb
Matej Hrica <xhrica@fi.muni.cz>
2022-02-17 20:38:10 +0100

    pipewire screencast working

--- src/video_capture/screen_pipewire.cpp
+++ src/video_capture/screen_pipewire.cpp
@@ -6,6 +6,7 @@
 #include <gio/gunixfdlist.h>
 #include <thread>
 #include <future>
+#include <chrono>
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
@@ -14,6 +15,7 @@
 #include <spa/param/video/format-utils.h>
 #include <spa/param/props.h>
 #include <spa/debug/format.h>
+#include <atomic>
 
 #include "debug.h"
 #include "host.h"
@@ -143,24 +145,30 @@ static void portal_call(GDBusConnection *connection, GDBusProxy *screencast_prox
 }
 
 struct screen_cast_session {
-        GDBusConnection *connection = nullptr;
-        GDBusProxy *screencast_proxy = nullptr;
-        int pipewire_fd = -1;
-        uint32_t pipewire_node = -1;
-        std::promise<void> screen_cast_is_ready;
+    GDBusConnection *connection = nullptr;
+    GDBusProxy *screencast_proxy = nullptr;
+    int pipewire_fd = -1;
+    uint32_t pipewire_node = -1;
 
-        struct pw_thread_loop *loop = nullptr;
-        struct pw_core *core = nullptr;
+    struct pw_thread_loop *loop = nullptr;
+    struct pw_core *core = nullptr;
 
-        struct pw_stream *stream = nullptr;
-        struct spa_hook stream_listener = {};
-        struct spa_hook core_listener = {};
+    struct pw_stream *stream = nullptr;
+    struct spa_hook stream_listener = {};
+    struct spa_hook core_listener = {};
 
-        struct spa_io_position *position = nullptr;
+    struct spa_io_position *position = nullptr;
 
-        struct spa_video_info format = {};
-        int32_t stride = 0;
-        struct spa_rectangle size = {};
+    struct spa_video_info format = {};
+    //int32_t output_line_size = 0;
+    struct spa_rectangle size = {};
+
+    // TODO: atomically exchange the pointer instead of a mutex?
+    std::mutex frame_mutex;
+    //struct video_frame *frame;
+    struct video_frame *frame;
+    std::promise<void> screen_cast_is_ready;
+    
 };
 
 static void on_stream_state_changed(void *_data, enum pw_stream_state old,
@@ -192,13 +200,93 @@ static void on_stream_io_changed(void *_data, uint32_t id, void *area, uint32_t 
     }
 }
 
+#define MAX_BUFFERS  64
+
 static void on_stream_param_changed(void *session_ptr, uint32_t id, const struct spa_pod *param) {
     auto &session = *static_cast<screen_cast_session*>(session_ptr);
     (void) id;
     std::cout << "[cap_pipewire] param changed:\n" << std::endl;
     spa_debug_format(2, nullptr, param);
-    pw_stream_update_params(session.stream, &param, 1);
 
+    // from example code, not sure what this is
+    if (param == NULL || id != SPA_PARAM_Format)
+        return;
+
+    int parse_format_ret = spa_format_parse(param, &session.format.media_type, &session.format.media_subtype);
+    assert(parse_format_ret > 0);
+
+    assert(session.format.media_type == SPA_MEDIA_TYPE_video);
+    assert(session.format.media_subtype == SPA_MEDIA_SUBTYPE_raw);
+
+    spa_format_video_raw_parse(param, &session.format.info.raw);
+    session.size = SPA_RECTANGLE(session.format.info.raw.size.width,
+                                session.format.info.raw.size.height);
+    std::cout << "size: " << session.format.info.raw.size.width << " x " << session.format.info.raw.size.height
+                << std::endl;
+    int mult = 1;
+
+    int linesize = vc_get_linesize(session.size.width, RGBA);
+    int32_t size = linesize * session.size.height;
+
+    uint8_t params_buffer[1024];
+
+    struct spa_pod_builder builder = SPA_POD_BUILDER_INIT(params_buffer, sizeof(params_buffer));
+    const struct spa_pod *params[5];
+
+    params[0] = static_cast<spa_pod *>(spa_pod_builder_add_object(&builder,
+        SPA_TYPE_OBJECT_ParamBuffers, SPA_PARAM_Buffers,
+        SPA_PARAM_BUFFERS_buffers,
+        SPA_POD_CHOICE_RANGE_Int(8, 2, MAX_BUFFERS),
+        SPA_PARAM_BUFFERS_blocks, SPA_POD_Int(1),
+        SPA_PARAM_BUFFERS_size, SPA_POD_Int(size * mult),
+        SPA_PARAM_BUFFERS_stride,
+        SPA_POD_Int(linesize * mult),
+        SPA_PARAM_BUFFERS_align, SPA_POD_Int(16),
+        SPA_PARAM_BUFFERS_dataType,
+        SPA_POD_CHOICE_FLAGS_Int((1 << SPA_DATA_MemPtr)))
+    );
+
+    /* a header metadata with timing information */
+    params[1] = static_cast<spa_pod *>(spa_pod_builder_add_object(&builder,
+        SPA_TYPE_OBJECT_ParamMeta, SPA_PARAM_Meta,
+        SPA_PARAM_META_type, SPA_POD_Id(SPA_META_Header),
+        SPA_PARAM_META_size,
+        SPA_POD_Int(sizeof(struct spa_meta_header)))
+    );
+    /* video cropping information */
+    params[2] = static_cast<spa_pod *>(spa_pod_builder_add_object(&builder,
+        SPA_TYPE_OBJECT_ParamMeta, SPA_PARAM_Meta,
+        SPA_PARAM_META_type, SPA_POD_Id(SPA_META_VideoCrop),
+        SPA_PARAM_META_size,
+        SPA_POD_Int(sizeof(struct spa_meta_region)))
+    );
+    
+    #define CURSOR_META_SIZE(w, h)   (sizeof(struct spa_meta_cursor) + sizeof(struct spa_meta_bitmap) + (w) * (h) * 4)
+    /* cursor information */
+    params[3] = static_cast<spa_pod *>(spa_pod_builder_add_object(&builder,
+        SPA_TYPE_OBJECT_ParamMeta, SPA_PARAM_Meta,
+        SPA_PARAM_META_type, SPA_POD_Id(SPA_META_Cursor),
+        SPA_PARAM_META_size, SPA_POD_CHOICE_RANGE_Int(
+            CURSOR_META_SIZE(64, 64),
+            CURSOR_META_SIZE(1, 1),
+            CURSOR_META_SIZE(256, 256))
+        )
+    );
+
+    pw_stream_update_params(session.stream, params, 4);
+}
+
+void copy_bgra_to_rgba(char *dest, char *src, int width, int height) {
+    int linesize = 4*width;
+    for (int line_offset = 0; line_offset < height * linesize; line_offset += linesize) {
+        for(int x = 0; x < 4*width; x += 4) {
+            // rgba <- bgra
+            dest[line_offset + x    ] = src[line_offset + x + 2];
+            dest[line_offset + x + 1] = src[line_offset + x + 1];
+            dest[line_offset + x + 2] = src[line_offset + x    ];
+            dest[line_offset + x + 3] = src[line_offset + x + 3];
+        }
+    }
 }
 
 static void on_process(void *session_ptr) {
@@ -221,6 +309,42 @@ static void on_process(void *session_ptr) {
         pw_log_warn("out of buffers: %m");
         return;
     }
+
+    struct video_frame* frame = vf_alloc(1);
+    frame->color_spec = RGBA; //TODO: or BGRx (BGRA)?
+    frame->interlacing = PROGRESSIVE;
+    frame->fps = 30; //TODO
+
+    
+    struct tile* tile = vf_get_tile(frame, 0);
+    tile->width = session.size.width; //TODO
+    tile->height = session.size.height; //TODO
+
+    tile->data_len = vc_get_linesize(tile->width, frame->color_spec) * tile->height;
+    tile->data = (char *) malloc(tile->data_len);
+    
+    char *src = static_cast<char*>(buffer->buffer->datas[0].data);
+    char *dest = tile->data;
+    int linesize = buffer->buffer->datas[0].chunk->stride;
+    
+    assert(linesize == vc_get_linesize(tile->width, frame->color_spec));
+    if (src == nullptr) {
+        std::cout<<"no data"<<std::endl;
+        pw_stream_queue_buffer(stream, buffer);
+        return;
+    }
+
+    copy_bgra_to_rgba(dest, src, session.size.width, session.size.height);
+    //memcpy(dest, src, session.size.height*linesize);
+    
+    //copy memory while converting BGRA -> RGBA
+    
+    std::lock_guard<std::mutex> frame_guard(session.frame_mutex);
+    // if the frame is nullptr it hasn't been returned by grab, 
+    // but we already have a newer frame so we free it and update the frame to a newer one
+    vf_free(session.frame); //this is noop if frame is nullptr
+
+    session.frame = frame;
 
     pw_stream_queue_buffer(stream, buffer);
 }
@@ -263,15 +387,21 @@ static void on_core_done_cb(void *user_data, uint32_t id, int seq) {
 
 static const struct pw_core_events core_events = {
         PW_VERSION_CORE_EVENTS,
+        .info = nullptr,
         .done = on_core_done_cb,
+        .ping = nullptr,
         .error = on_core_error_cb,
+        .remove_id = nullptr,
+        .bound_id = nullptr,
+        .add_mem = nullptr,
+        .remove_mem = nullptr
 };
 
 const struct spa_pod *params[2];
 
 static int start_pipewire(screen_cast_session &session)
 {
-    auto *params_buffer = static_cast<uint8_t *>(malloc(1024));
+    uint8_t params_buffer[1024];
     struct spa_pod_builder pod_builder = SPA_POD_BUILDER_INIT(params_buffer, sizeof(params_buffer));
 
     session.loop = pw_thread_loop_new("pipewire_thread_loop", nullptr);
@@ -318,8 +448,8 @@ static int start_pipewire(screen_cast_session &session)
             SPA_FORMAT_mediaSubtype, SPA_POD_Id(SPA_MEDIA_SUBTYPE_raw),
             SPA_FORMAT_VIDEO_format,
             SPA_POD_CHOICE_ENUM_Id(
-                    4, /*SPA_VIDEO_FORMAT_RGB15, */SPA_VIDEO_FORMAT_RGB, SPA_VIDEO_FORMAT_BGRx, SPA_VIDEO_FORMAT_YUY2,
-                    SPA_VIDEO_FORMAT_YUV9 /*, SPA_VIDEO_FORMAT_RGB SPA_VIDEO_FORMAT_RGBA_F32, SPA_VIDEO_FORMAT_RGBA_F32, SPA_VIDEO_FORMAT_BGRA, SPA_VIDEO_FORMAT_RGBA,
+                    2, SPA_VIDEO_FORMAT_BGRA, SPA_VIDEO_FORMAT_BGRx /*SPA_VIDEO_FORMAT_RGB15, SPA_VIDEO_FORMAT_RGB, SPA_VIDEO_FORMAT_YUY2,
+                    SPA_VIDEO_FORMAT_YUV9 , SPA_VIDEO_FORMAT_RGB SPA_VIDEO_FORMAT_RGBA_F32, SPA_VIDEO_FORMAT_RGBA_F32, SPA_VIDEO_FORMAT_BGRA, SPA_VIDEO_FORMAT_RGBA,
                     SPA_VIDEO_FORMAT_RGBx*/),
             SPA_FORMAT_VIDEO_size,
             SPA_POD_CHOICE_RANGE_Rectangle(
@@ -344,16 +474,14 @@ static int start_pipewire(screen_cast_session &session)
         fprintf(stderr, "can't connect: %s\n", spa_strerror(res));
         return -1;
     }
- 
+    std::cout<<"thread loop unlock"<<std::endl;
     pw_thread_loop_unlock(session.loop);
+    std::cout<<"thread loop unlocked"<<std::endl;
     return 0;
 }
 
-static void run_screencast(std::promise<void> screen_cast_is_ready) {
-    screen_cast_session session = {.screen_cast_is_ready = std::move(screen_cast_is_ready)};
-    
-    //struct screen_cast_session session{};
-    // session.screen_cast_is_ready = std::move(screen_cast_is_ready);
+static void run_screencast(screen_cast_session *session_ptr) {
+    auto& session = *session_ptr;
 
     session.pipewire_fd = -1;
     session.pipewire_node = -1;
@@ -455,7 +583,7 @@ static void run_screencast(std::promise<void> screen_cast_is_ready) {
 
         portal_call(session.connection, session.screencast_proxy, session_handle, "SelectSources",
                     {
-                            {"types",    g_variant_new_uint32(1)}, // 1 full screen, 2 - a window, 3 - both
+                            {"types",    g_variant_new_uint32(3)}, // 1 full screen, 2 - a window, 3 - both
                             {"multiple", g_variant_new_boolean(false)}
                     },
                     sources_selected
@@ -490,15 +618,16 @@ static struct vidcap_type * vidcap_screen_pipewire_probe(bool verbose, void (**d
 static int vidcap_screen_pipewire_init(struct vidcap_params *params, void **state)
 {
     (void) params;
-    (void) state;
+    
+    screen_cast_session *session = new screen_cast_session();
+    *state = session;
     
     std::cout<<"[cap_pipewire] init\n";
     pw_init(&uv_argc, &uv_argv);
 
-    std::promise<void> screen_cast_is_ready_promise;
-    std::future<void> screen_cast_is_ready = screen_cast_is_ready_promise.get_future();
-    std::thread dbus_thread(run_screencast, std::move(screen_cast_is_ready_promise));
-    screen_cast_is_ready.wait();
+    std::future<void> ready = session->screen_cast_is_ready.get_future();
+    std::thread dbus_thread(run_screencast, session);
+    ready.wait();
     dbus_thread.detach();
     return VIDCAP_INIT_OK;
 }
@@ -509,22 +638,27 @@ static void vidcap_screen_pipewire_done(void *state)
     std::cout<<"[cap_pipewire] done\n";
 }
 
-static struct video_frame *vidcap_screen_pipewire_grab(void *state, struct audio_frame **audio)
+static struct video_frame *vidcap_screen_pipewire_grab(void *session_ptr, struct audio_frame **audio)
 {
-    (void) audio;
-    (void) state;
-    //std::cout<<"[cap_pipewire] grab\n";
-
-    //exit(0);
-    return nullptr;
+    //TODO: dont send a frame that has already been sent
+    assert(session_ptr != nullptr);
+    //usleep(1000);
+    auto &session = *static_cast<screen_cast_session*>(session_ptr);
+    audio = nullptr;
+    std::lock_guard<std::mutex> frame_guard(session.frame_mutex);
+    struct video_frame *frame = session.frame;
+    if (frame != nullptr)
+        std::cout<<"grab"<<std::endl;
+    session.frame = nullptr;
+    return frame;
 }
 
 static const struct video_capture_info vidcap_screen_pipewire_info = {
-        vidcap_screen_pipewire_probe,
-        vidcap_screen_pipewire_init,
-        vidcap_screen_pipewire_done,
-        vidcap_screen_pipewire_grab,
-        false, //TODO
+    vidcap_screen_pipewire_probe,
+    vidcap_screen_pipewire_init,
+    vidcap_screen_pipewire_done,
+    vidcap_screen_pipewire_grab,
+    false, //TODO what is this?
 };
 
 REGISTER_MODULE(screen_pipewire, &vidcap_screen_pipewire_info, LIBRARY_CLASS_VIDEO_CAPTURE, VIDEO_CAPTURE_ABI_VERSION);
